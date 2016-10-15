b336b91796df8fa9ada9e80b5677db1113f2f6b4
taimoorshehzad <taimoorshehzad28@gmail.com>
2016-10-16 02:21:49 +0500

    First Commit

---
+++ SocialApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialApp.Startup))]
+namespace SocialApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
