6e3727bd4cdde383f18a7ffa48b85d94e4e632b5
Behzad Bhatti <hafizbehzad132@gmail.com>
2016-11-27 12:57:58 +0500

    Missing DashboardBL file

---
+++ SocialApp.BL/DashboardBL.cs
@@ -0,0 +1,35 @@
+﻿using SocailApp.Repository;
+using SocialApp.DB.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialApp.BL
+{
+    public class DashboardBL
+    {
+        public List<OrganizationUsersViewModel> DashboardStats() 
+        {
+
+            IUserProfileRepository userProfileRepository = new UserProfileRepository();
+            IOrganizationRepository organizationRepository = new OrganizationRepository();
+
+            List<OrganizationUsersViewModel> viewModelList = new List<OrganizationUsersViewModel>();
+
+            var organizations = organizationRepository.Get();
+            foreach (var item in organizations)
+            {
+                OrganizationUsersViewModel viewModel = new OrganizationUsersViewModel();
+                viewModel.OrganizationID = item.OrganizationID;
+                viewModel.OrganizationName = item.Name;
+                viewModel.UsersCount = userProfileRepository.Get().Where(s => s.OrganizationID == item.OrganizationID).Count();
+
+                viewModelList.Add(viewModel);
+            }
+            return viewModelList;
+        }
+
+    }
+}
